27590c43689f0129afb84ccdf8de20ae798e2eb4
skyline2080 <rusantonov80@rambler.ru>
2022-08-05 22:24:05 +0300

    triangle hierarchy revised to reduce mem footprint

--- Shape/Shape.cs
+++ Shape/Shape.cs
@@ -1,4 +1,6 @@
-﻿namespace Shape;
+﻿using System;
+
+namespace Shape;
 
 // решил использовать float, т.к. double предоставляет избыточную в данном случае точность
 
@@ -27,28 +29,30 @@ public class Triangle: IArea {
             : new Triangle (leg1, leg2, leg3) 
         : throw new ArgumentException("passed arguments do not represent a proper triangle");
 
-    public float Leg1 {get; }
-    public float Leg2 {get; }
-    public float Leg3 {get; }
+    public float Leg1 { get => _leg1; }
+    public float Leg2 { get => _leg2; }
+    public float Leg3 { get => _leg3; }
     public virtual float Area {
         get {
-            var p = (Leg1 + Leg2 + Leg3) / 2;
+            var p = (_leg1 + _leg2 + _leg3) / 2;
             return 
                 MathF.Sqrt(
                     (p) * 
-                    (p - Leg1) * 
-                    (p - Leg2) * 
-                    (p - Leg3));
+                    (p - _leg1) * 
+                    (p - _leg2) * 
+                    (p - _leg3));
         }
     }
+
+    protected float _leg1, _leg2, _leg3;
 
     protected Triangle(float leg1, float leg2, float leg3) {
         if (! is_proper_triangle(leg1, leg2, leg3))
             throw new ArgumentException("passed arguments do not represent a proper triangle");
         
-        Leg1 = leg1;
-        Leg2 = leg2;
-        Leg3 = leg3;
+        _leg1 = leg1;
+        _leg2 = leg2;
+        _leg3 = leg3;
     }
 
     protected static bool is_proper_triangle(float leg1, float leg2, float leg3) {
@@ -73,26 +77,26 @@ public class Triangle: IArea {
 }
 
 public sealed class RightTriangle: Triangle, IArea {
-    public static new RightTriangle WithLegs(float leg1, float leg2, float leg3) => 
-        new RightTriangle(leg1, leg2, leg3);
-    
-    public float Hypot {get; } 
-    public float Cat1  {get; } 
-    public float Cat2  {get; }
+    public float Hypot { get => _leg1; } 
+    public float Cat1  { get => _leg2; } 
+    public float Cat2  { get => _leg3; }
     public override float Area => Cat1 * Cat2 / 2;
 
+    internal static new RightTriangle WithLegs(float leg1, float leg2, float leg3) => 
+        new RightTriangle(leg1, leg2, leg3);
+    
     RightTriangle(float leg1, float leg2, float leg3)
-        : base (leg1, leg2, leg3) {
-        
+        : base (leg1, leg2, leg3) 
+    {
         if (! Triangle.is_right_triangle(leg1, leg2, leg3))
             throw new ArgumentException("passed arguments do not represent a right triangle");
 
         Span<float> mem = stackalloc float[3] {leg1, leg2, leg3};
         mem.Sort();
         
-        Hypot = mem[2];
-        Cat1  = mem[1];
-        Cat2  = mem[0];
+        _leg1 = mem[2];
+        _leg2 = mem[1];
+        _leg3 = mem[0];
     
     }
 }
